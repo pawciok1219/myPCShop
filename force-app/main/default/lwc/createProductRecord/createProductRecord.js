a8958a6b3e5d8f5ea4a2e09d29b4113987a66295
pawciok1219 <pawciok1219@gmail.com>
2022-05-26 09:32:54 +0200

    fix bug with standard pricebook Id

--- force-app/main/default/lwc/createProductRecord/createProductRecord.js
+++ force-app/main/default/lwc/createProductRecord/createProductRecord.js
@@ -1,6 +1,7 @@
 import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { createRecord } from 'lightning/uiRecordApi';
+import getStandardPricebookId from '@salesforce/apex/filePreviewAndDownloadController.getStandardPricebookId';
 
 export default class CreateProductRecord extends LightningElement {
     @track productPrice;
@@ -34,20 +35,23 @@ export default class CreateProductRecord extends LightningElement {
             variant: "success"
         });
         this.dispatchEvent(toastEvent);
-
         this.Id = recordId;
-        let fields = {'Pricebook2Id' : '01s7Q000006w2QqQAI', 'Product2Id' : recordId, 'UnitPrice' : this.productPrice};
-        let objRecordInput = {'apiName' : 'PricebookEntry', fields};
-        createRecord(objRecordInput).then(response => {
-        }).catch(error => {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error in adding the price!',
-                    message: error.body.message,
-                    variant: 'error',
-                }),
-            );
-        });
+
+        getStandardPricebookId()
+		.then(result => {
+            let fields = {'Pricebook2Id' : result, 'Product2Id' : recordId, 'UnitPrice' : this.productPrice};
+            let objRecordInput = {'apiName' : 'PricebookEntry', fields};
+            createRecord(objRecordInput).then(response => {
+            }).catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error in adding the price!',
+                        message: error.body.message,
+                        variant: 'error',
+                    }),
+                );
+            });
+		})
         this.isModalImageOpen = true;
     }
 
